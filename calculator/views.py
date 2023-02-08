ebdc87ca02992780c8bb27571cdff9559360bac5
asiripragada <92725571+asiripragada@users.noreply.github.com>
2023-02-08 18:00:02 +0530

    Update views.py

--- calculator/views.py
+++ calculator/views.py
@@ -1,37 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-import pandas as pd
+#from rest_framework.response import Response
 import json
 
+
+## Load the required libraries
+import numpy as np
+import pandas as pd
+
+import os
+import openai
 # Create your views here.
+import os
+OPENAI_API_KEY= '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'
+
+def Merge(dict1, dict2):
+    return(dict2.update(dict1))
 
 def index(request):
     return render(request, "input.html")
 
 
 def addition(request):
+    method = request.GET['method']
+    text = request.GET['text']
 
-    num1 = request.GET['num1']
-    num2 = request.GET['num2']
 
-    if num1.isdigit() and num2.isdigit():
-        a = int(num1)
-        b = int(num2)
-        res = a + b
-  
+    if not text.isdigit():
+    
+        text='food is good'
+        openai.api_key = OPENAI_API_KEY
         
-        # initialize list of lists
-#         data = [['output', res]]
-          
-        # Create the pandas DataFrame
-#         df = pd.DataFrame(data, columns=['key', 'value'])
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt="Decide whether a Tweet's sentiment is positive, neutral, or negative.\n\nTweet:"+"'"+text+"'"+"\nSentiment:",
+          temperature=0,
+          max_tokens=60,
+          top_p=1.0,
+          frequency_penalty=0.5,
+          presence_penalty=0.0,
+        )
+        
+        sent={'method':'Sentiment'}
 
-        result = pd.DataFrame({'bla':[1,2,3],'bla2':['a','b','c']}).to_json(orient='records')
-        return JsonResponse(json.loads(result), safe = False)
+        Merge(sent,response)
+        return JsonResponse(response, safe = False)
 
-        #return HttpResponse(int(res))
-#         return JsonResponse({'key':int(res)})
+        #return render(request, "result.html", {"result": predictions1})
     else:
         res = "Only digits are allowed"
         return render(request, "result.html", {"result": res})
